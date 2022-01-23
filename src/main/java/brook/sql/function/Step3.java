9d0dae8e559bc4f6a21b132cea53e499b28e5992
dongwei <jackblack369@163.com>
2022-01-23 17:29:39 +0800

    add flink sql over&window&join

--- src/main/java/brook/sql/function/Step3.java
+++ src/main/java/brook/sql/function/Step3.java
@@ -40,37 +40,57 @@ public class Step3 {
                 " tran_direct STRING,\n" +
                 " target_account STRING,\n" +
                 " tran_type_id INT,\n" +
-                "WATERMARK FOR trans_date_time AS trans_date_time -1 INTERVAL '1' MINUTE\n" +
+                "WATERMARK FOR trans_date_time AS trans_date_time + INTERVAL '10' MINUTE\n" +
                 ") WITH (\n" +
                 "'connector' = 'kafka',\n" +
                 "'topic' = 'tran_source',\n" +
                 "'properties.bootstrap.servers' = '127.0.0.1:9092',\n" +
                 "'format' = 'json',\n" +
-                "'scan.startup.mode'='latest-offset'\n" +
+                "'scan.startup.mode'='earliest-offset'\n" +
                 ")";
 
         String initKafkaData = "insert into tran_source " +
                 "select trans_id,TO_TIMESTAMP(CONCAT_WS(' ', CAST(trans_date as VARCHAR), CAST(trans_time as VARCHAR)), 'yyyy-MM-dd HH:mm:ss') as trans_date_time, account, amount, tran_direct, target_account, tran_type_id from test_source";
 
         String dimensionSQL = "create table tran_type_table(\n" +
-                " tran_type_id, \n" +
-                " Tran_type_name \n" + //交易类型名(1:转账2:汇款3:消费4:工资5:其他) )
+                " tran_type_id INT, \n" +
+                " tran_type_name STRING, \n" + //交易类型名(1:转账2:汇款3:消费4:工资5:其他) )
+                " update_time TIMESTAMP(3), \n" +
+//                " update_time TIMESTAMP(3) METADATA FROM `values.source.timestamp` VIRTUAL,\n" +
+                " primary key(tran_type_id) not enforced, \n" +
+                " WATERMARK FOR update_time AS update_time \n" +
                 ") WITH (\n" +
-                "'connector' = 'kafka',\n" +
-                "'topic' = 'tran_source',\n" +
+                "'connector' = 'upsert-kafka',\n" +
+                "'topic' = 'tran_type_table',\n" +
                 "'properties.bootstrap.servers' = '127.0.0.1:9092',\n" +
-                "'format' = 'json',\n" +
-                "'scan.startup.mode'='latest-offset'\n" +
+//                "'format' = 'json',\n" +
+//                "'scan.startup.mode'='earliest-offset',\n" +
+                "'key.format' = 'json',\n" +
+                "'value.format' = 'json'\n" +
                 ")";
-
 
         tableEnv.executeSql(datagenSQL);
         tableEnv.executeSql(kafkaSQL);
-
-        //        tableEnv.executeSql("select * from test_source").print();
-        //        tableEnv.executeSql(testSQL).print();
-
         tableEnv.executeSql(initKafkaData);
+
+        tableEnv.executeSql(dimensionSQL);
+        String initDimensionDataSQL = "insert into tran_type_table (tran_type_id,tran_type_name, update_time) values (1,'转账', LOCALTIMESTAMP),(2,'汇款',LOCALTIMESTAMP),(3,'消费',LOCALTIMESTAMP),(4,'工资',LOCALTIMESTAMP),(5,'其他',LOCALTIMESTAMP)";
+        tableEnv.executeSql(initDimensionDataSQL);
+
+
+//        String initDimensionDataSQL = "insert into tran_type_table values (1,'one')，(2,'two'),(3,'three'),(4,'four'),(5,'five')";
+
+//        tableEnv.executeSql("select * from tran_type_table").print();
+
+        tableEnv.executeSql("select a.trans_id, a.tran_type_id,a.trans_date_time, b.tran_type_id, b.tran_type_name, b.update_time " +
+                " from tran_source as a" +
+                " left join tran_type_table FOR SYSTEM_TIME AS OF a.trans_date_time as b" +
+//                " left join tran_type_table FOR SYSTEM_TIME AS OF PROCTIME() as b" +
+                " on a.tran_type_id = b.tran_type_id").print();
+
+//        String initDimensionDataSQL2 = "insert into tran_type_table (tran_type_id,tran_type_name) values (1,'转账'),(2,'汇款'),(3,'消费'),(4,'工资'),(5,'其他')";
+//        tableEnv.executeSql(initDimensionDataSQL2);
+
 
         env.execute();
     }
